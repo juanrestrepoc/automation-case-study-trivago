db67d768fe281b4d3a0271c0975db94710362f5c
Juan Camilo Restrepo <camilo.restrepo@hotschedules.com>
2016-09-22 17:03:44 -0500

    Initial maven project setup+}

---
+++ src/main/java/com/trivago/casestudy/App.java
@@ -0,0 +1,13 @@
+package com.trivago.casestudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
